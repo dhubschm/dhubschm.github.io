fc8369213dc81027b13f102c729d8aec9a42f3d3
Dominic Hubschmitt <dhubschm@uci.edu>
2026-04-22 03:31:19 -0700

    almost done, may add future project sections, need to go back and edit new experiences

--- homepage.js
+++ homepage.js
@@ -2,3 +2,12 @@ function toggleAccordion(item) {
   const content = item.querySelector('.accordion-content');
   content.classList.toggle('open');
 }
+function openResume() {
+  document.getElementById('resumeOverlay').classList.add('open');
+  document.body.style.overflow = 'hidden';
+}
+
+function closeResume() {
+  document.getElementById('resumeOverlay').classList.remove('open');
+  document.body.style.overflow = '';
+}
